c73295dff2eaeb53339416815ea59f4b2305d78e
dCrowb <oishevchenko94@gmail.com>
2023-08-13 19:24:27 +0300

    part 1

---
+++ test_connect.py
@@ -0,0 +1,21 @@
+import configparser
+from pymongo.mongo_client import MongoClient
+
+config =  configparser.ConfigParser()
+config.read('config.ini')
+
+mongo_user = config.get('DB', 'user')
+mongodb_pass = config.get('DB', 'pass')
+db_name = config.get('DB', 'db_name')
+domain = config.get('DB', 'domain')
+
+uri = f'mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/?retryWrites=true&w=majority'
+
+client = MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+    
